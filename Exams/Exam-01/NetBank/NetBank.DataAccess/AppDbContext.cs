ac7bf0bbb6043d41eec66b10fe48c24e789b8d29
DanlerLo <loaizad65@gmail.com>
2022-03-19 01:50:46 -0500

    Hola

---
+++ Exams/Exam-01/NetBank/NetBank.DataAccess/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace NetBank.DataAccess.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using NetBank.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ReportedCard> ReportedCards { get; set; }
+    }
+}
