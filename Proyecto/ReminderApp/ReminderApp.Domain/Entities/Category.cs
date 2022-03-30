87459b44836e93c2a4f93516a0e95c641788dcd9
DanlerLo <loaizad65@gmail.com>
2022-03-29 19:37:00 -0500

    Hola

---
+++ Proyecto/ReminderApp/ReminderApp.Domain/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ReminderApp.Domain.Entities
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Reminders = new HashSet<Reminder>();
+        }
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Reminder> Reminders { get; set; }
+    }
+}
---
+++ Proyecto/ReminderApp/ReminderApp.Domain/Entities/Reminder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ReminderApp.Domain.Entities
+{
+    public partial class Reminder
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public string CronExpression { get; set; }
+        public int? NumberOfTime { get; set; }
+        public bool? Enabled { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
