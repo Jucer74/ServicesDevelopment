ebd57549c53b90c0b0d81306f7e50ec5c930e516
DanlerLo <loaizad65@gmail.com>
2022-04-01 21:19:04 -0500

    Holis

---
+++ Proyecto/ReminderApp/ReminderApp.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReminderApp.Domain.Entities;
+using ReminderApp.Infraestucture.Context;
+using ReminderApp.Infrastructure.Common;
+
+
+namespace ReminderApp.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>
+    {
+        public CategoryRepository(AppDBContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
---
+++ Proyecto/ReminderApp/ReminderApp.Infrastructure/Repositories/RimenderRepository.cs
@@ -0,0 +1,22 @@
+﻿using ReminderApp.Domain.Entities;
+using ReminderApp.Infraestucture.Context;
+using ReminderApp.Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReminderApp.Infrastructure.Repositories
+{
+    public class RimenderRepository : Repository<Reminder>
+    {
+        public RimenderRepository(AppDBContext appDbContext) : base(appDbContext)
+        {
+        }
+        public IEnumerable<Reminder>FindReminderByCategory(Category category)
+        {
+            return base.Find(c => c.Category.Equals(category));
+        }
+    }
+}
