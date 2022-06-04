46a5164f71c412e3267b259cd1bf2f12bd496319
Julio Robles <Jucer74@Hotmail.com>
2022-06-04 03:59:39 -0500

    Exam-03

---
+++ Exams/Exam-03/PricatApp/Pricat.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pricat.Infrastructure
+{
+   public class Class1
+   {
+   }
+}
---
+++ Exams/Exam-03/Scripts/02_Tab_Categories.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `pricatdb`.`categories` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Description` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`Id`));
+
