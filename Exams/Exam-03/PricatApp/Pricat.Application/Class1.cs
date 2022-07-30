c06acd84da20ca589bb54c2e0cf9ba2b91ee60d6
Julio Robles <Jucer74@Hotmail.com>
2022-07-29 19:09:29 -0500

    Update

--- Exams/Exam-03/PricatApp/Pricat.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Pricat.Application
-{
-   public class Class1
-   {
-   }
-}
--- Exams/Exam-03/PricatApp/Pricat.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Pricat.Infrastructure
-{
-   public class Class1
-   {
-   }
-}
