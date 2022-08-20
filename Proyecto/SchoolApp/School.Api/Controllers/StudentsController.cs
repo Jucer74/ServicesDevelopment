8546042503a50865e5ae7f34e2ab783d56ade89d
Julio Robles <Jucer74@Hotmail.com>
2022-08-19 21:48:43 -0500

    Initial API

---
+++ Proyecto/SchoolApp/School.Api/Controllers/StudentsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace School.Api.Controllers
+{
+   [Route("api/v1.0/[controller]")]
+   [ApiController]
+   public class StudentsController : ControllerBase
+   {
+      // GET: api/<StudentsController>
+      [HttpGet]
+      public IEnumerable<Student> Get()
+      {
+         var students = new List<Student>()
+         {
+            new Student()
+            {
+               Id = 1,
+               FirstName = "John",
+               LastName = "Doe",
+               DateOfBirth = DateTime.Parse("10-08-1974"),
+               Sex = 'M'
+            }
+         };
+
+         return students;
+         //return new string[] { "value1", "value2" };
+      }
+
+      // GET api/<StudentsController>/5
+      [HttpGet("{id}")]
+      public string Get(int id)
+      {
+         return "value";
+      }
+
+      // POST api/<StudentsController>
+      [HttpPost]
+      public void Post([FromBody] string value)
+      {
+      }
+
+      // PUT api/<StudentsController>/5
+      [HttpPut("{id}")]
+      public void Put(int id, [FromBody] string value)
+      {
+      }
+
+      // DELETE api/<StudentsController>/5
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+      }
+   }
+}
