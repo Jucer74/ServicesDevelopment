c2447de4eaff50c95d23fbe17cb999047360c08c
JUAN <JUANCHO1198@GMAIL.COM>
2022-08-26 18:35:56 -0500

    Summary

---
+++ Proyecto/StudentsApp/School.Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public char Sex { get; set; }
+
+    }
+}
