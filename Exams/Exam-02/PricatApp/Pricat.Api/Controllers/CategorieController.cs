9b9e85c3160ff798866348990e5eefa0d07d8a03
Juan Pablo Sanchez Robledo <juancho1198@gmail.com>
2022-10-14 14:21:17 -0500

    Actu4

---
+++ Exams/Exam-02/PricatApp/Pricat.Api/Controllers/CategorieController.cs
@@ -0,0 +1,68 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using PricatApp.Application.Interfaces;
+using PricatApp.Domain.Entities;
+using PricatApp.Domain.Exceptions;
+
+namespace Pricat.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategorieController : ControllerBase
+    {
+
+        private readonly ICategorieService _categorieService;
+        public CategorieController(ICategorieService categorieService)
+        {
+            _categorieService = categorieService;
+        }
+
+        // GET: api/<CategorieController>
+        [HttpGet]
+        //[ProducesDefaultResponseType((int)HttpStatusCode.OK, Type= typeof(List<category>))]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _categorieService.GetAllAsync());
+        }
+
+        // GET api/<CategorieController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _categorieService.GetByIdAsync(id));
+        }
+
+        // POST api/<CategorieController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Categorie categorie)
+        {
+            return Ok(await _categorieService.AddAsync(categorie));
+        }
+
+        // PUT api/<CategorieController
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, [FromBody] Categorie categorie)
+        {
+            try
+            {
+                return Ok(await _categorieService.UpdateAsync(id, categorie));
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // DELETE api/<CategorieController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categorieService.RemoveAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ Exams/Exam-02/PricatApp/Pricat.Api/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using PricatApp.Application.Interfaces;
+using PricatApp.Domain.Entities;
+using PricatApp.Domain.Exceptions;
+
+namespace PricatApp.Api.Controllers
+{
+
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productService.GetAllAsync());
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _productService.GetByIdAsync(id));
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Product product)
+        {
+            return Ok(await _productService.AddAsync(product));
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Product product)
+        {
+            try
+            {
+                return Ok(await _productService.UpdateAsync(id, product));
+            }
+            catch (BadRequestException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.RemoveAsync(id);
+            return Ok();
+        }
+    }
+}
