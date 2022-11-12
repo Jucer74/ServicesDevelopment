20b73b88b7fea07420acce43f59d3037c2ccefb7
Juan Pablo Sanchez Robledo <juancho1198@gmail.com>
2022-11-11 20:45:31 -0500

    EP

---
+++ Practices/08.Services/PeopleApi-main/Code/PeopleApp/People.Api/Controllers/PeopleController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using People.Application.Interfaces;
+using People.Domain.Entities;
+using People.Domain.Exceptions;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace People.Api.Controllers
+{
+   [Route("api/v1/[controller]")]
+   [ApiController]
+   public class PeopleController : ControllerBase
+   {
+      private readonly IPersonService _personService;
+
+      public PeopleController(IPersonService personService)
+      {
+         _personService = personService;
+      }
+
+      // GET: api/<PeopleController>
+      [HttpGet]
+      public async Task<IActionResult> GetAll()
+      {
+         return Ok(await _personService.GetAllAsync());
+      }
+
+      // GET api/<PeopleController>/5
+      [HttpGet("{id}")]
+      public async Task<IActionResult> GetById(int id)
+      {
+         try
+         {
+            return Ok(await _personService.GetByIdAsync(id));
+         }
+         catch (NotFoundException ex)
+         {
+            return NotFound(ex.Message);
+         }
+      }
+
+      // POST api/<PeopleController>
+      [HttpPost]
+      public async Task<IActionResult> Post([FromBody] Person person)
+      {
+         return Ok(await _personService.AddAsync(person));
+      }
+
+      // PUT api/<PeopleController>/5
+      [HttpPut("{id}")]
+      public async Task<IActionResult> Put(int id, [FromBody] Person person)
+      {
+         try
+         {
+            return Ok(await _personService.UpdateAsync(id, person));
+         }
+         catch (BadRequestException ex)
+         {
+            return BadRequest(ex.Message);
+         }
+         catch (NotFoundException ex)
+         {
+            return NotFound(ex.Message);
+         }
+      }
+
+      // DELETE api/<PeopleController>/5
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> Delete(int id)
+      {
+         try
+         {
+            await _personService.RemoveAsync(id);
+            return Ok();
+         }
+         catch (NotFoundException ex)
+         {
+            return NotFound(ex.Message);
+         }
+      }
+   }
+}
---
+++ Practices/08.Services/PeopleApi-main/Code/PeopleApp/People.Application/Services/PersonService.cs
@@ -0,0 +1,77 @@
+﻿using People.Application.Interfaces;
+using People.Domain.Entities;
+using People.Domain.Exceptions;
+using People.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace People.Application.Services
+{
+   public class PersonService : IPersonService
+   {
+      private readonly IPersonRepository _personRepository;
+
+      public PersonService(IPersonRepository personRepository)
+      {
+         _personRepository = personRepository;
+      }
+
+      public async Task<Person> AddAsync(Person entity)
+      {
+         return await _personRepository.AddAsync(entity);
+      }
+
+      public async Task<IEnumerable<Person>> FindAsync(Expression<Func<Person, bool>> predicate)
+      {
+         return await _personRepository.FindAsync(predicate);
+      }
+
+      public async Task<IEnumerable<Person>> GetAllAsync()
+      {
+         return await _personRepository.GetAllAsync();
+      }
+
+      public async Task<Person> GetByIdAsync(int id)
+      {
+         var person = await _personRepository.GetByIdAsync(id);
+
+         if (person is null)
+         {
+            throw new NotFoundException($"Person with Id={id} Not Found");
+         }
+         
+         return person;
+      }
+
+      public async Task RemoveAsync(int id)
+      {
+         var person = await _personRepository.GetByIdAsync(id);
+
+         if (person is null)
+         {
+            throw new NotFoundException($"Person with Id={id} Not Found");
+         }
+
+         await _personRepository.RemoveAsync(person);
+      }
+
+      public async Task<Person> UpdateAsync(int id, Person entity)
+      {
+         if (id != entity.Id)
+         {
+            throw new BadRequestException($"The Id={id} not corresponding with Entity.Id={entity.Id}");
+         }
+
+         var person = await _personRepository.GetByIdAsync(id);
+
+         if (person is null)
+         {
+            throw new NotFoundException($"Person with Id={id} Not Found");
+         }
+
+         return (await _personRepository.UpdateAsync(entity));
+      }
+   }
+}
