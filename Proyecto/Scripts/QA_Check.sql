64ea840374cbdca45ffb40371c44a6f39872f9ca
Julio Robles <Jucer74@Hotmail.com>
2023-06-01 04:40:16 -0500

    Update Data and Scripts

---
+++ Proyecto/Scripts/QA_Check.sql
@@ -0,0 +1,4 @@
+SELECT Count(1) 'Customers' FROM arepasdb.customers;
+SELECT Count(1) 'Products' FROM arepasdb.products;
+SELECT Count(1) 'Orders' FROM arepasdb.orders;
+SELECT Count(1) 'OrderDetails' FROM arepasdb.orderdetails;
