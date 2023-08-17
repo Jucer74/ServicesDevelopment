436b8303ea08ab4adb76aee4a7cfa7a783fb669d
Sagudeloz <agudelosan2002@gmail.com>
2023-08-17 10:48:54 -0500

    a

---
+++ Practices/02.CleanArchitecture/StudentsApp/StudentsApp.Api/Extensions/ModuleCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using StudentsApp.Application.Interfaces;
+using StudentsApp.Application.Services;
+using StudentsApp.Domain.Interfaces.Repositories;
+using StudentsApp.Infrastructure.Repositories;
+
+namespace StudentsApp.Api.Extensions;
+
+public static class ModuleCollectionExtensions
+{
+    public static IServiceCollection AddCoreModules(this IServiceCollection services)
+    {
+        // Services / Use Cases
+        services.AddScoped<IStudentService, StudentService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddInfrastructureModules(this IServiceCollection services)
+    {
+        // Repositories
+        services.AddScoped<IStudentRepository, StudentRepository>();
+
+        return services;
+    }
+}
