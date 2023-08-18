19cc0c4242036c05fa65b8057bef38180d1457c2
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-08-18 09:07:22 -0500

    Add the infrastructure layer

---
+++ Practices/02.CleanArchitecture/StudentsApp/Students.Infrastructure/Repositories/PersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using Students.Domain.Entities;
+using Students.Domain.Interfaces.Repositories;
+using Students.Infrastructure.Common;
+using Students.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students.Infrastructure.Repositories
+{
+    public class PersonRepository : Repository<Student>, IStudentRepository
+    {
+        public PersonRepository(AppDbContext appDbContext):base(appDbContext) { }
+    }
+}
