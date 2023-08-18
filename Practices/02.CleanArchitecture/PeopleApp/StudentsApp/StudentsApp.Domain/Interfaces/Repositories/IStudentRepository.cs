e8652f22312ba82aa430a6bc94d4c52ae5f25ea6
brandon5254 <83289428+brandon5254@users.noreply.github.com>
2023-08-18 03:54:05 -0500

    Revert "creacion proyecto arquitectura clean"
    This reverts commit 8ff775897c375f1bdcee987b5ad68063175dfad4.

--- Practices/02.CleanArchitecture/PeopleApp/StudentsApp/StudentsApp.Domain/Interfaces/Repositories/IStudentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using StudentsApp.Domain.Common;
-using StudentsApp.Domain.Entities;
-using System.Linq.Expressions;
-
-namespace StudentsApp.Domain.Interfaces.Repositories;
-
-    public interface IStudentRepository : IRepository<Student>
-{
-}
