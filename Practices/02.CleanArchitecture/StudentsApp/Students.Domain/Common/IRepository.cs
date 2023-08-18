0f0cdd5eef3666be847c5cdf56439964d6517067
sntgrstrp <107089380+sntgrstrp@users.noreply.github.com>
2023-08-18 14:30:49 -0500

    StudentsApp

---
+++ Practices/02.CleanArchitecture/StudentsApp/Students.Domain/Common/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace People.Domain.Common
+{
+   public interface IRepository<T> where T : EntityBase
+   {
+      public Task<T> AddAsync(T entity);
+
+      public Task<IEnumerable<T>> GetAllAsync();
+
+      public Task<T> GetByIdAsync(int id);
+
+      public Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+      public Task<T> UpdateAsync(T entity);
+
+      public Task RemoveAsync(T entity);
+   }
+}
---
+++ Practices/02.CleanArchitecture/StudentsApp/Students.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace People.Domain.Exceptions
+{
+   [Serializable]
+   public class NotFoundException : Exception
+   {
+      public NotFoundException()
+      {
+      }
+
+      public NotFoundException(string message) : base(message)
+      {
+      }
+
+      public NotFoundException(string message, Exception innerException)
+          : base(message, innerException)
+      {
+      }
+
+      // Without this constructor, deserialization will fail
+      protected NotFoundException(SerializationInfo info, StreamingContext context)
+          : base(info, context)
+      {
+      }
+   }
+}
