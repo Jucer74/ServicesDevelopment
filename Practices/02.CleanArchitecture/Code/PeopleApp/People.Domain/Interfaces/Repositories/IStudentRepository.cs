ae783faa99d31ccf41a32658e9a9f252bca91553
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-08-18 20:06:02 -0500

    a

--- Practices/02.CleanArchitecture/Code/PeopleApp/People.Domain/Interfaces/Repositories/IStudentRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Student.Domain.Common;
-using Student.Domain.Entities;
-
-namespace Student.Domain.Interfaces.Repositories
-{
-   public interface IStudentRepository : IRepository<Entities.Student>
-   {
-   }
-}
