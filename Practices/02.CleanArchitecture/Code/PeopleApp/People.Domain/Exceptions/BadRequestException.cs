d5b5dbe6a57d59ed59c33ee3bcaeb765a26e0a5f
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-08-18 19:01:52 -0500

    ClonacionDedeJulroburi

---
+++ Practices/02.CleanArchitecture/Code/PeopleApp/People.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace People.Domain.Exceptions
+{
+   [Serializable]
+   public class BadRequestException : Exception
+   {
+      public BadRequestException()
+      {
+      }
+
+      public BadRequestException(string message) : base(message)
+      {
+      }
+
+      public BadRequestException(string message, Exception innerException)
+          : base(message, innerException)
+      {
+      }
+
+      // Without this constructor, deserialization will fail
+      protected BadRequestException(SerializationInfo info, StreamingContext context)
+          : base(info, context)
+      {
+      }
+   }
+}
---
+++ Practices/02.CleanArchitecture/README.md
@@ -0,0 +1,18 @@
+# PeopleApi
+Repositorio que ejemplifica la creacion de una API usando .Net
+
+# Proyecto
+1. Se crea una solucion en blanco llamada **PeopleApp**
+2. Dentro de esta solucion cree la estructura de Carpetas referenciando **Clean Architecture**
+
+![](https://github.com/Jucer74/PeopleApi/blob/main/Images/Clean-Architecture-Diagram.jpg)
+
+	- Api
+	- Application
+	- Domain
+	- Infrastructure
+
+3. En la Capa de Api, cree un proyecto de tipo **ASP .NET Core Web API** llamado **People.Api**
+4. En las demas capas cree proyectos de tipo **Class Library** correspondientes a cada capa
+
+
