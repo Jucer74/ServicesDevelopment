6bff625f1d8af9a36bc5c9e12050f3bd31eaf940
Santierazo <103299493+Santierazo@users.noreply.github.com>
2023-08-18 19:35:49 -0500

    a

--- Practices/02.CleanArchitecture/StudentsApp/StudentsApp.Api/Startup.cs
+++ Practices/02.CleanArchitecture/StudentsApp/StudentsApp.Api/Startup.cs
@@ -27,7 +27,7 @@ public class Startup
         services.AddControllers();
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudentApp.Api", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "PeopleApp.Api", Version = "v1" });
         });
 
         // Add Modules
@@ -44,7 +44,7 @@ public class Startup
         {
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StudentApp.Api v1"));
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PeopleApp.Api v1"));
         }
 
         app.UseHttpsRedirection();
