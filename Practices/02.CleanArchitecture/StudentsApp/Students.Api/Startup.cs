4e70869eb6c850e3abfb1611ead7f37e7945bf7e
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-08-20 12:59:25 -0500

    Fix the service bug

--- Practices/02.CleanArchitecture/StudentsApp/Students.Api/Startup.cs
+++ Practices/02.CleanArchitecture/StudentsApp/Students.Api/Startup.cs
@@ -1,62 +1,65 @@
-using Microsoft.OpenApi.Models;
-using Students.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Students.Api.Extensions;
+using Students.Application.Interfaces;
+using Students.Application.Services;
+using Students.Infrastructure.Context;
 
-namespace Students.Api
+namespace Students.Api;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration) 
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseMySQL(Configuration.GetConnectionString("StudentsDB")));
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
         {
-            Configuration = configuration;
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Students.Api", Version = "v1" });
+        });
+
+
+        // Add Modules
+        services.AddCoreModules();
+        services.AddInfrastructureModules();
+
+
+        services.AddCors();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Students.Api v1"); });
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseHttpsRedirection();
 
-        public void ConfigureServices(IServiceCollection services)
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseCors(options =>
         {
+            options.WithOrigins("http://localhost:3000");
+            options.AllowAnyMethod();
+            options.AllowAnyHeader();
+        });
 
-            services.AddDbContext<AppDbContext>(options => options.UseSqlite("Name=StudentsDB"));
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Students.Api", Version = "v1" });
-            });
-
-            // Add Modules
-            services.AddCoreModules();
-            services.AddInfrastructureModules();
-
-
-            services.AddCors();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseEndpoints(endpoints =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Students.Api v1");});
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseCors(options =>{
-                options.WithOrigins("http://localhost:3000");
-                options.AllowAnyMethod();
-                options.AllowAnyHeader();
-            });
-
-            app.UseEndpoints(endpoints => {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
     }
 }
