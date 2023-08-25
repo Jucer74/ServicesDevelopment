f54512d5c4e19e86295f27740448e9f42c6f4de0
Abc <bjulian127@gmail.com>
2023-08-25 18:38:47 -0500

    Cambiando el students un poco

---
+++ Practices/02.CleanArchitecture/StudentsApp/StudentsApp.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsApp.Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
