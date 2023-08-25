03f6b2729255f17e657a0d33374f8b00c1eb9a87
Julio Robles <Jucer74@Hotmail.com>
2023-08-25 18:37:37 -0500

    Exam-01

---
+++ Exams/Exam-01/NwetBankApp/NetBank.Domain/Common/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetBank.Domain.Common
+{
+   public abstract class EntityBase
+   {
+      [Key]
+      [Required]
+      public int Id { get; set; }
+   }
+}
---
+++ Exams/Exam-01/NwetBankApp/NetBank.Domain/Dto/IssuingNetworkData.cs
@@ -0,0 +1,9 @@
+﻿namespace NetBank.Domain.Dto;
+
+public class IssuingNetworkData
+{
+    public string Name { get; set; } = null!;
+    public List<int>? StartsWithNumbers { get; set; } = null!;
+    public RangeNumber? InRange { get; set; } = null!;
+    public List<int> AllowedLengths { get; set; } = null!;
+}
