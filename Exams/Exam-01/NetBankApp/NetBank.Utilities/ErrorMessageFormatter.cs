e48f6332fdfff6a6778c4e519e09725307fdfee4
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-08-30 20:55:38 -0500

    Add the error-formatter

---
+++ Exams/Exam-01/NetBankApp/NetBank.Utilities/ErrorMessageFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NetBank.Utilities;
+
+public static class ErrorMessageFormatter
+{
+    public static string AddSpaceBetweenLowerAndCapitalLetter(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+
+        string pattern = @"(?=\p{Lu})(?<=\p{Ll})";
+
+        string result = Regex.Replace(input, pattern, " ");
+
+        return result;
+    }
+}
