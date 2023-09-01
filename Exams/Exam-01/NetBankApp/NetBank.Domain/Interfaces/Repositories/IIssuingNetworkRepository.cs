a950c3aede23adbdced6b27f1ba8d7b6a1860814
Abc <bjulian127@gmail.com>
2023-08-31 21:21:44 -0500

    Subirlo again

---
+++ Exams/Exam-01/NetBankApp/NetBank.Domain/Interfaces/Repositories/IIssuingNetworkRepository.cs
@@ -0,0 +1,13 @@
+﻿using NetBank.Domain.Common;
+using NetBank.Domain.Models;
+
+namespace NetBank.Domain.Interfaces.Repositories
+{
+    public interface IIssuingNetworkRepository : IRepository<IssuingNetwork>
+    {
+        List<IssuingNetwork> GetAllIssuingNetworks();
+
+
+    }
+
+}
---
+++ Exams/Exam-01/NetBankApp/Netbank.Application/Interfaces/ICreditCardService.cs
@@ -0,0 +1,14 @@
+﻿using NetBank.Domain.Define;
+using NetBank.Domain.Dto;
+
+namespace Netbank.Application.Interfaces
+{
+
+    public interface ICreditCardService
+    {
+        string IdentifyIssuingNetwork(string creditCardNumber);
+
+        public Task<ValidationResultType> Validate(string creditCardNumber);
+        public CreditCardResult Result { get; set; }
+    }
+}
