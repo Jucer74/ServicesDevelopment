7d27c23cadc64f95b6797c3b9f1caa9dc4a3b60f
arcethekiller <Penarandaalexander18@hotmail.com>
2023-09-01 16:39:11 -0500

    Exxamn/01

--- Exams/Exam-01/NetBankApp/Netbank.Application/Services/CreditCardService.cs
+++ Exams/Exam-01/NetBankApp/Netbank.Application/Services/CreditCardService.cs
@@ -4,6 +4,8 @@ using NetBank.Domain.Dto;
 using NetBank.Domain.Interfaces.Repositories;
 using NetBank.Domain.Models;
 using System.Linq;
+using NetBank.Utilities;
+using System.Text.RegularExpressions;
 
 namespace Netbank.Application.Services;
 
@@ -22,6 +24,8 @@ public class CreditCardService : ICreditCardService
 
     public CreditCardResult Result { get; set; } = null!;
 
+
+
     #endregion Properties
 
     public CreditCardService(IIssuingNetworkRepository issuingNetworkRepository)
@@ -30,23 +34,153 @@ public class CreditCardService : ICreditCardService
     }
 
     public async Task<ValidationResultType> Validate(string creditCardNumber)
-    {
-        // List<IssuingNetworkData> issuingNetworkDataList = await LoadIssuingNetworkData();
 
-        // Call the Individual Validations
+    {
+
+        bool isValid = false; //CreditCardValidator.IsValid(creditCardNumber);
+        string nombreTarjeta = " ";
+
+
+        //Traer la los datos de la DB
+        List<IssuingNetwork> issuingNetworksList = await GetIssuingNetworks();
+
+        if (Regex.IsMatch(creditCardNumber, NUMBER_REGEX))
+        {
+            foreach (IssuingNetwork issuingNetwork in issuingNetworksList)
+            {
+                if (detectarRedEmisora(creditCardNumber, issuingNetwork))
+                {
+
+                    nombreTarjeta = issuingNetwork.Name;
+
+                    if (VerificarLongitud(creditCardNumber, issuingNetwork.AllowedLengths))
+                    {
+                        isValid = CreditCardValidator.IsValid(creditCardNumber);
+                        Result = new CreditCardResult(nombreTarjeta, isValid);
+                        return ValidationResultType.Ok;
+
+                    }
+                    else
+                    {
+
+                        Result = new CreditCardResult(nombreTarjeta, isValid);
+                        return ValidationResultType.Ok;
+                    }
+
+                }
+
+
+
+            }
+            isValid = false;
+            nombreTarjeta = "Not Found";
+            Result = new CreditCardResult(nombreTarjeta, isValid);
+            return ValidationResultType.NotFound;
+
+
+        }
+        else
+        {
+            isValid = false;
+            nombreTarjeta = "Bad Request";
+            Result = new CreditCardResult(nombreTarjeta, isValid);
+            return ValidationResultType.BadRequest;
+
+        }
+
+
+
+
+
 
         throw new NotImplementedException();
     }
 
-    private async Task<List<IssuingNetworkData>> LoadIssuingNetworkData()
-    {
-        // Convert Data to List Data
-        throw new NotImplementedException();
-    }
+
+
+
+
 
     private async Task<List<IssuingNetwork>> GetIssuingNetworks()
     {
-        // Load Data From DataBase
-        throw new NotImplementedException();
+        var issuingNetworks = await _issuingNetworkRepository.GetAllAsync();
+        return issuingNetworks.ToList();
     }
+
+    
+
+    static bool IsNumeroEnRango(string numeroTarjeta, string rango)
+    {
+        if (string.IsNullOrEmpty(rango) || string.IsNullOrEmpty(numeroTarjeta))
+        {
+            return false;
+        }
+
+        string[] limites = rango.Split('-');
+
+        if (limites.Length != 2 || !(Regex.IsMatch(limites[0], NUMBER_REGEX)) || !Regex.IsMatch(limites[0], NUMBER_REGEX))
+        {
+            return false;
+        }
+
+        int limiteInferior = int.Parse(limites[0]);
+        int limiteSuperior = int.Parse(limites[1]);
+
+        if (Regex.IsMatch(numeroTarjeta, NUMBER_REGEX))
+        {
+            int numeroTarjetaEntero = int.Parse(numeroTarjeta.Substring(0, limites[0].Length));
+            return numeroTarjetaEntero >= limiteInferior && numeroTarjetaEntero <= limiteSuperior;
+        }
+
+        return false;
+    }
+
+    static bool detectarRedEmisora(string cardNumber, IssuingNetwork tarjeta)
+    {
+
+
+
+        if (tarjeta.InRange != null)
+        {
+            if (IsNumeroEnRango(cardNumber, tarjeta.InRange))
+            {
+                return true;
+            }
+
+        }
+
+        if (inicioNumero(cardNumber, tarjeta.StartsWithNumbers))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    static bool inicioNumero(string cardNumber, string startingNumbers)
+    {
+        if (string.IsNullOrEmpty(startingNumbers))
+        {
+            return false;
+        }
+
+        string[] startingNumbersList = startingNumbers.Split(',');
+        return startingNumbersList.Any(startingNumber => cardNumber.StartsWith(startingNumber.Trim()));
+    }
+
+
+
+    static bool VerificarLongitud(string numeroTarjeta, string longitudesPermitidas)
+    {
+        var longitudes = longitudesPermitidas.Split(',');
+        int longitudTarjeta = numeroTarjeta.Length;
+
+        return longitudes.Any(longitud => longitudTarjeta == int.Parse(longitud));
+    }
+
+
+
+
+
 }
