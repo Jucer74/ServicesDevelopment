a3104b5c5d580b7132e79f230aba14323ee96f88
ja23cardona1406 <jaimeandrescardonam@gmail.com>
2023-09-01 17:59:48 -0500

    examen

---
+++ Exams/Exam-01/NetBankApp/Netbank.Application/Mapp/IssuingNetworkData.cs
@@ -0,0 +1,69 @@
+﻿using NetBank.Domain.Dto;
+using NetBank.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Netbank.Application.Mappers
+{
+    public static class IssuingNetworkDataMapper
+    {
+        public static List<IssuingNetworkData> MapIssuingNetworksToData(List<IssuingNetwork> issuingNetworks)
+        {
+            List<IssuingNetworkData> issuingNetworkDataList = new();
+
+            foreach (IssuingNetwork issuingNetwork in issuingNetworks)
+            {
+                IssuingNetworkData issuingNetworkData = MapIssuingNetworkToData(issuingNetwork);
+                issuingNetworkDataList.Add(issuingNetworkData);
+            }
+
+            return issuingNetworkDataList;
+        }
+
+        public static IssuingNetworkData MapIssuingNetworkToData(IssuingNetwork issuingNetwork)
+        {
+            return new IssuingNetworkData
+            {
+                Name = issuingNetwork.Name,
+                StartsWithNumbers = ConvertNumbersToList(issuingNetwork.StartsWithNumbers),
+                InRange = CreateRangeNumberObject(issuingNetwork.InRange),
+                AllowedLengths = ConvertNumbersToList(issuingNetwork.AllowedLengths)
+            };
+        }
+
+        private static List<int> ConvertNumbersToList(string? numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return new List<int>();
+            }
+
+            List<string> numbersStringList = numbers.Split(',').Select(num => num.Trim()).ToList();
+            List<int> numbersIntList = numbersStringList.Select(int.Parse).ToList();
+
+            return numbersIntList;
+        }
+
+        private static RangeNumber? CreateRangeNumberObject(string? inRange)
+        {
+            if (string.IsNullOrEmpty(inRange))
+            {
+                return null;
+            }
+
+            string[] rangeParts = inRange.Trim().Split('-');
+
+            if (rangeParts.Length != 2)
+            {
+                throw new ArgumentException("Invalid range format.");
+            }
+
+            return new RangeNumber
+            {
+                MinValue = int.Parse(rangeParts[0]),
+                MaxValue = int.Parse(rangeParts[1])
+            };
+        }
+    }
+}
--- Exams/Exam-01/NetBankApp/Netbank.Application/Services/CreditCardService.cs
+++ Exams/Exam-01/NetBankApp/Netbank.Application/Services/CreditCardService.cs
@@ -1,52 +1,96 @@
-﻿using Netbank.Application.Interfaces;
+﻿using NetBank.Application.Interfaces;
 using NetBank.Domain.Define;
 using NetBank.Domain.Dto;
 using NetBank.Domain.Interfaces.Repositories;
 using NetBank.Domain.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-namespace Netbank.Application.Services;
-
-public class CreditCardService : ICreditCardService
+namespace NetBank.Application.Services
 {
-    #region Loval-Vars
-
-    private readonly IIssuingNetworkRepository _issuingNetworkRepository;
-
-    // Regular Expression To Validate Only Numbers
-    private const string NUMBER_REGEX = "^[0-9]*$";
-
-    #endregion Loval-Vars
-
-    #region Properties
-
-    public CreditCardResult Result { get; set; } = null!;
-
-    #endregion Properties
-
-    public CreditCardService(IIssuingNetworkRepository issuingNetworkRepository)
+    public class CreditCardValidationService : ICreditCardValidationService
     {
-        _issuingNetworkRepository = issuingNetworkRepository;
-    }
+        private readonly IIssuingNetworkRepository _issuingNetworkRepository;
+        private const string NUMBER_REGEX = "^[0-9]*$";
 
-    public async Task<ValidationResultType> Validate(string creditCardNumber)
-    {
-        // List<IssuingNetworkData> issuingNetworkDataList = await LoadIssuingNetworkData();
+        public CreditCardValidationService(IIssuingNetworkRepository issuingNetworkRepository)
+        {
+            _issuingNetworkRepository = issuingNetworkRepository;
+        }
 
-        // Call the Individual Validations
+        public async Task<ValidationResult> ValidateCreditCardAsync(string creditCardNumber)
+        {
+            bool isValidCreditCardLength;
+            bool isCreditCardNumberValid;
+            string issuingNetworkName;
+            List<IssuingNetworkData> issuingNetworkDataList = await LoadIssuingNetworkDataAsync();
 
-        throw new NotImplementedException();
-    }
+            if (!Regex.IsMatch(creditCardNumber, NUMBER_REGEX))
+            {
+                return ValidationResult.BadRequest("Invalid credit card number format.");
+            }
 
-    private async Task<List<IssuingNetworkData>> LoadIssuingNetworkData()
-    {
-        // Convert Data to List Data
-        throw new NotImplementedException();
-    }
+            issuingNetworkName = FindIssuingNetwork(creditCardNumber, issuingNetworkDataList);
 
-    private async Task<List<IssuingNetwork>> GetIssuingNetworks()
-    {
-        // Load Data From DataBase
-        throw new NotImplementedException();
+            if (string.IsNullOrEmpty(issuingNetworkName))
+            {
+                return ValidationResult.NotFound("Issuing network not found.");
+            }
+
+            isValidCreditCardLength = IsValidCreditCardLength(creditCardNumber, issuingNetworkDataList, issuingNetworkName);
+            isCreditCardNumberValid = IsValidCreditCardNumber(creditCardNumber);
+
+            if (isValidCreditCardLength && isCreditCardNumberValid)
+            {
+                return ValidationResult.Ok(issuingNetworkName, true);
+            }
+
+            return ValidationResult.Ok(issuingNetworkName, false);
+        }
+
+        private async Task<List<IssuingNetworkData>> LoadIssuingNetworkDataAsync()
+        {
+            List<IssuingNetwork> issuingNetworkList = await GetIssuingNetworksAsync();
+            return issuingNetworkList.Select(MapIssuingNetworkToData).ToList();
+        }
+
+        private async Task<List<IssuingNetwork>> GetIssuingNetworksAsync()
+        {
+            return (await _issuingNetworkRepository.GetAllAsync()).ToList();
+        }
+
+        private string FindIssuingNetwork(string creditCardNumber, List<IssuingNetworkData> issuingNetworkDataList)
+        {
+            // Implementa tu lógica para encontrar la red emisora aquí.
+            // Puedes utilizar la lista issuingNetworkDataList y el número de tarjeta de crédito proporcionado.
+            // Devuelve el nombre de la red emisora o null si no se encuentra.
+            return "ExampleNetwork";
+        }
+
+        private bool IsValidCreditCardLength(string creditCardNumber, List<IssuingNetworkData> issuingNetworkDataList, string issuingNetworkName)
+        {
+            // Implementa tu lógica para validar la longitud de la tarjeta de crédito aquí.
+            // Puedes utilizar la lista issuingNetworkDataList y el nombre de la red emisora proporcionado.
+            // Devuelve true si la longitud es válida, de lo contrario, false.
+            return true;
+        }
+
+        private bool IsValidCreditCardNumber(string creditCardNumber)
+        {
+            // Implementa tu lógica para validar el número de tarjeta de crédito aquí.
+            // Puedes utilizar el número de tarjeta de crédito proporcionado.
+            // Devuelve true si el número es válido, de lo contrario, false.
+            return true;
+        }
+
+        private IssuingNetworkData MapIssuingNetworkToData(IssuingNetwork issuingNetwork)
+        {
+            // Implementa tu lógica de mapeo aquí.
+            // Puedes mapear un objeto IssuingNetwork a un objeto IssuingNetworkData.
+            return new IssuingNetworkData();
+        }
     }
 }
