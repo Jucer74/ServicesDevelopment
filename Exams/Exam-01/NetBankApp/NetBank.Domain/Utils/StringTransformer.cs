8ca2579ec537e9eda3ac176b966302e8deb4076d
Abc <bjulian127@gmail.com>
2023-09-01 10:57:09 -0500

    30/30

---
+++ Exams/Exam-01/NetBankApp/NetBank.Domain/Utils/StringTransformer.cs
@@ -0,0 +1,99 @@
+﻿using NetBank.Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Domain
+{
+    public class StringTransformer
+    {
+        public static List<int>? ComaSeparatedValuesToIntList(string csv)
+        {
+            List<int>? intList = null;
+            if (csv != null)
+            {
+                intList = new List<int>();
+                List<string> stringList = SeparatedValuesToStringList(csv, ',');
+                foreach (var str in stringList)
+                {
+                    int? intValue = StringToInt(str);
+                    if (intValue != null)
+                    {
+                        intList.Add(intValue.Value);
+                    }
+                }
+            }
+            return intList;
+        }
+
+        public static RangeNumber? HyphenSeparatedValuesToRangeNumber(string hsv)
+        {
+            RangeNumber? rangeNumber = null;
+            if (hsv != null)
+            {
+                List<int> intList = HyphenSeparatedValuesToIntList(hsv);
+                
+                if (intList.Count == 2)
+                {
+                    rangeNumber = new RangeNumber();
+                    rangeNumber.MinValue = intList[0];
+                    rangeNumber.MaxValue = intList[1];
+                }
+            }
+            return rangeNumber;
+        }
+
+        public static List<int> HyphenSeparatedValuesToIntList(string hsv)
+        {
+            List<int> intList = new();
+            List<string> stringList = SeparatedValuesToStringList(hsv, '-');
+            foreach (var str in stringList)
+            {
+                int? num = StringToInt(str);
+                if (num != null)
+                {
+                    intList.Add(num.Value);
+                }
+            }
+            return intList;
+        }
+
+        public static List<string> SeparatedValuesToStringList(string sv, char separator)
+        {
+            List<string> stringList = sv.Split(new char[] { separator }).ToList();
+            return stringList;
+        }
+
+        public static int? StringToInt(string str)
+        {
+            int? num;
+            try
+            {
+                num = int.Parse(str);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                num = null;
+            }
+            return num;
+        }
+
+        public static Double? StringToDoble(string str)
+        {
+            Double? num;
+            try
+            {
+                num = Double.Parse(str);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                num = null;
+            }
+            return num;
+        }
+    }
+}
---
+++ Exams/Exam-01/NetBankApp/Netbank.Application/Mappers/IssuingNetworkMapper.cs
@@ -0,0 +1,35 @@
+﻿using NetBank.Domain;
+using NetBank.Domain.Dto;
+using NetBank.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netbank.Application.Mappers
+{
+    public class CreditCardMapper
+    {
+        public static IssuingNetworkData ToIssuingNetworkData(IssuingNetwork issuignNetwork)
+        {
+            IssuingNetworkData issuingNetworkData = new();
+            issuingNetworkData.Name = issuignNetwork.Name;
+            issuingNetworkData.StartsWithNumbers = StringTransformer.ComaSeparatedValuesToIntList(issuignNetwork.StartsWithNumbers);
+            issuingNetworkData.InRange = StringTransformer.HyphenSeparatedValuesToRangeNumber(issuignNetwork.InRange);
+            issuingNetworkData.AllowedLengths = StringTransformer.ComaSeparatedValuesToIntList(issuignNetwork.AllowedLengths);
+            return issuingNetworkData;
+
+        }
+
+        public static List<IssuingNetworkData> ToIssuingNetworkDataList(List<IssuingNetwork> issuingNetworks) {
+            List<IssuingNetworkData> issuingNetworkDataList = new();
+            foreach(IssuingNetwork issuingNetwork in issuingNetworks)
+            {
+                IssuingNetworkData issuignNetworkData = ToIssuingNetworkData(issuingNetwork);
+                issuingNetworkDataList.Add(issuignNetworkData);
+            }
+            return issuingNetworkDataList;
+        }
+    }
+}
