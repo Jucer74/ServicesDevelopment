667e785f5f3dbdb00f436f8f775ec61a7bc51937
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-09-01 20:38:27 -0500

    Correcion pc
    correcion pc

--- Exams/Exam-01/NetBankApp/NetBank.Infrastructure/Repositories/IssuingNetworkRepository.cs
+++ Exams/Exam-01/NetBankApp/NetBank.Infrastructure/Repositories/IssuingNetworkRepository.cs
@@ -1,8 +1,8 @@
-﻿using NetBank.Domain.Interfaces.Repositories;
+﻿using NetBank.Domain.Dto;
+using NetBank.Domain.Interfaces.Repositories;
 using NetBank.Domain.Models;
 using NetBank.Infrastructure.Common;
 using NetBank.Infrastructure.Context;
-
 
 namespace NetBank.Infrastructure.Repositories
 {
@@ -10,6 +10,16 @@ namespace NetBank.Infrastructure.Repositories
     {
         public IssuingNetworkRepository(AppDbContext appDbContext) : base(appDbContext)
         {
+        }
+
+        Task<List<IssuingNetworkData>> IIssuingNetworkRepository.GetIssuingNetworkDataFromDatabaseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<IssuingNetwork>> IIssuingNetworkRepository.GetIssuingNetworksAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
