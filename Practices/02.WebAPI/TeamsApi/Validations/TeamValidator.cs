1c88e631d0a12b7a19ff41818ba39598cf077a66
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-09-08 20:49:15 -0500

    .
    .

--- Practices/02.WebAPI/TeamsApi/Validations/TeamValidator.cs
+++ Practices/02.WebAPI/TeamsApi/Validations/TeamValidator.cs
@@ -7,6 +7,8 @@ public class TeamValidator: AbstractValidator<TeamDto>
 {
     public TeamValidator()
     {
-        RuleFor(m  => m.Name).NotEmpty();
+        RuleFor(m  => m.Name).NotEmpty().WithMessage("El name es requerido")
+            .Length(50).WithMessage("La longitud maxima del nombre es 50 car");
+        RuleFor(m => m.Coach).NotEmpty().WithMessage("El name es requerido");
     }
 }
