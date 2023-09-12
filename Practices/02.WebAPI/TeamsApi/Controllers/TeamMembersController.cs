7b203f90e7ae89cfc0434fc3fabae0d19ac9e6f0
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-12 09:07:15 -0500

    Create the TeamMembersConstroller

---
+++ Practices/02.WebAPI/TeamsApi/Controllers/TeamMembersController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TeamsApi.Dtos;
+using TeamsApi.Models;
+using TeamsApi.Services;
+
+namespace TeamsApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TeamMembersController:ControllerBase
+{
+    private readonly ITeamMemberService _teamMemberService;
+    private readonly IMapper _mapper;
+
+    public TeamMembersController(ITeamMemberService teamMemberService, IMapper mapper)
+    {
+        _teamMemberService = teamMemberService;
+        _mapper = mapper;
+    }
+
+    // GET: api/<TeamMembersController>
+    [HttpGet]
+    public async Task<IActionResult> GetAllTeamMembers()
+    {
+        var teamMembers = await _teamMemberService.GetAllTeamMembers();
+        return Ok(_mapper.Map<List<TeamMember>, List<TeamMemberDto>>(teamMembers));
+    }
+
+
+    // GET api/<TeamMembersController>/5
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTeamMemberById(int id)
+    {
+        var teamMember = await _teamMemberService.GetTeamMemberById(id);
+        return Ok(_mapper.Map<TeamMember, TeamMemberDto>(teamMember));
+    }
+
+    // POST api/<TeamMembersController>
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] TeamMemberDto teamMemberDto)
+    {
+        TeamMember teamMember = await _teamMemberService.CreateTeamMember(_mapper.Map<TeamMemberDto, TeamMember>(teamMemberDto));
+
+        return Ok(_mapper.Map<TeamMember, TeamMemberDto>(teamMember));
+    }
+}
