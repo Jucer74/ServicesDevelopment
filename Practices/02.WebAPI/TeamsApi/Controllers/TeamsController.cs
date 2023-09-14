2cd1f8e11f967fd6dfa44280c87c8554de62ae4f
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-14 18:40:09 -0500

    Remove unused method

--- Practices/02.WebAPI/TeamsApi/Controllers/TeamsController.cs
+++ Practices/02.WebAPI/TeamsApi/Controllers/TeamsController.cs
@@ -37,14 +37,6 @@ namespace TeamsApi.Controllers
             return Ok(_mapper.Map<Team, TeamDto>(team));
         }
 
-        //GET api/<TeamsController>/5/members
-        [HttpGet("{id}/members")]
-        public async Task<IActionResult> GetTeamMembers(int id)
-        {
-            var teamMembers = await _teamService.GetTeamMembersByTeamId(id);
-            return Ok(_mapper.Map<List<TeamMember>, List<TeamMemberDto>>(teamMembers));
-        }
-
         // POST api/<TeamsController>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] TeamDto teamDto)
