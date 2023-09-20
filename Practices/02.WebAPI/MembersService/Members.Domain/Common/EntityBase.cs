f285b9afeef1b6e39c0bc9fda2aa19e0adbb6996
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-09-20 11:55:51 -0500

    Remove foreign keys

---
+++ Practices/02.WebAPI/MembersService/Members.Domain/Common/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace People.Domain.Common;
+
+public abstract class EntityBase
+{
+    [Key]
+    public int Id { get; set; }
+}
---
+++ Practices/02.WebAPI/MembersService/Members.Domain/Entities/Member.cs
@@ -0,0 +1,19 @@
+﻿using People.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Members.Domain.Entities;
+
+public class Member : EntityBase
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    public string LastName { get; set; } = null!;
+
+    public string Position { get; set; } = null!;
+
+
+    public int TeamId { get; set; }
+}
