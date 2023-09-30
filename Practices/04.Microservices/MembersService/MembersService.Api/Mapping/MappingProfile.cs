e5822f19e9b6ec5d0bba97f7f3a66ee175a833f5
arcethekiller <>
2023-09-29 21:23:52 -0500

    project

---
+++ Practices/04.Microservices/MembersService/MembersService.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MembersService.Domain.Entities;
+
+namespace MembersService.Api.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<MemberDto, Member>();
+        CreateMap<Member, MemberDto>();
+    }
+}
---
+++ Practices/04.Microservices/MembersService/MembersService.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MembersService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MembersService.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Member> Members { get; set; }
+}
