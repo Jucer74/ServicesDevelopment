202e4a17b415b1a3d308f52ee6c8be3d083d67be
davidhb7 <herrera17david@gmail.com>
2023-10-06 18:12:43 -0500

    Alineacion repaso incompleto

---
+++ Practices/02.WebAPI/TeamsApi/Dtos/TeamDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamsApi.Dtos;
+
+public class TeamDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Coach { get; set; } = null!;
+    public string Conference{ get; set; } = null!;
+}
---
+++ Practices/02.WebAPI/TeamsApi/Services/ITeamService.cs
@@ -0,0 +1,18 @@
+﻿using TeamsApi.Models;
+
+namespace TeamsApi.Services;
+
+public interface ITeamService
+{
+    Task<Team> CreateTeam(Team team);
+
+    Task DeleteTeam(int id);
+
+    Task<List<Team>> GetAllTeams();
+
+    Task<Team> GetTeamById(int id);
+
+    Task<Team> UpdateTeam(int id, Team team);
+
+    Task<List<TeamMember>> GetTeamMembersByTeamId(int id);
+}
