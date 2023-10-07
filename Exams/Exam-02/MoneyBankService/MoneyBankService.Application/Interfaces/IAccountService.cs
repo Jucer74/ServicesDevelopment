260ca11815db67ea606c75a35c5325a271ba5640
arcethekiller <Penarandaalexander18@hotmail.com>
2023-10-07 12:00:52 -0500

    ---
    Finish

--- Exams/Exam-02/MoneyBankService/MoneyBankService.Application/Interfaces/IAccountService.cs
+++ Exams/Exam-02/MoneyBankService/MoneyBankService.Application/Interfaces/IAccountService.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MoneyBankService.Api.Dto;
+using MoneyBankService.Domain.Entities;
+using System.Linq.Expressions;
+using System.Transactions;
 
 namespace MoneyBankService.Application.Interfaces;
 
 public interface IAccountService
 {
+    Task<Account> CreateAccount(Account account);
+    Task DeleteAccount(int id);
+    Task<IEnumerable<Account>> GetAllAccounts();
+    Task<Account> GetAccountById(int id);
+    Task<Account> UpdateAccount(int id, Account account);
+    Task Deposit(int id, TransactionDto transactionDto);
+    Task Withdraw(int id, TransactionDto transactionDto);
+
+    Task<IEnumerable<Account>> FindAccountsByAccountNumber(string accountNumber);
 }
---
+++ Exams/Exam-02/MoneyBankService/MoneyBankService.Domain/Dto/AccountDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MoneyBankService.Api.Dto;
+
+public class AccountDto
+{
+    public int Id { get; set; }
+    public char AccountType { get; set; } = 'A';
+    public DateTime CreationDate { get; set; } = DateTime.Now;
+    public string AccountNumber { get; set; } = null!;
+    public string OwnerName { get; set; } = null!;
+    public decimal BalanceAmount { get; set; }
+    public decimal OverdraftAmount { get; set; }
+}
