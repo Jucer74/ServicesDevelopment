c94c3dcc73d786b6ad6cc9e57bba42b0803deb3c
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-06 20:28:12 -0500

    Add account validator

--- Exams/Exam-02/MoneyBankService/MoneyBankService.Api/Validators/TransactionValidator.cs
+++ Exams/Exam-02/MoneyBankService/MoneyBankService.Api/Validators/TransactionValidator.cs
@@ -1,5 +1,27 @@
-﻿namespace MoneyBankService.Api.Validators;
+﻿using FluentValidation;
+using MoneyBankService.Api.Dto;
 
-public class TransactionValidator
+namespace MoneyBankService.Api.Validators;
+
+public class TransactionValidator : AbstractValidator<TransactionDto>
 {
+    public TransactionValidator() 
+    {
+        RuleFor(m => m.Id)
+            .NotEmpty()
+            .WithMessage("The Id is required.");
+
+        RuleFor(m => m.AccountNumber)
+            .NotEmpty()
+            .WithMessage("The AccountNumber is required.")
+            .MaximumLength(10)
+            .WithMessage("The AccountNumber has a maximum length of 10 characters.")
+            .Matches(@"\d{10}")
+            .WithMessage("The AccountNumber only accepts numbers.");
+
+        RuleFor(m => m.ValueAmount)
+            .NotEmpty()
+            .WithMessage("The ValueAmount is required.");
+        
+    }
 }
