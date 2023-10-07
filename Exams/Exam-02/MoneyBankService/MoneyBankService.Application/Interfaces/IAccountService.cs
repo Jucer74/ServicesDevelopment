1799c99b8c72000a961f6314d0936b29c49d7125
Santiago Gómez <santi.gomezg.13@gmail.com>
2023-10-06 19:41:39 -0500

    Complete the IAccountService

--- Exams/Exam-02/MoneyBankService/MoneyBankService.Application/Interfaces/IAccountService.cs
+++ Exams/Exam-02/MoneyBankService/MoneyBankService.Application/Interfaces/IAccountService.cs
@@ -1,5 +1,13 @@
-﻿namespace MoneyBankService.Application.Interfaces;
+﻿using MoneyBankService.Domain.Entities;
+
+namespace MoneyBankService.Application.Interfaces;
 
 public interface IAccountService
 {
+    Task<Account> CreateAccount(Account account);
+    Task DeleteAccount(int id);
+    Task<IEnumerable<Account>> GetAllAccounts();
+    Task<Account> GetAccountById(int id);
+    Task<Account> UpdateAccount(int id , Account account);
+
 }
