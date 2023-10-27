b3e68545fc15ba6b1bd3cbc061094e735387583b
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-10-27 16:15:00 -0500

    ..
    .

---
+++ Practices/04.Microservices/MembersService/MembersService.Application/Interfaces/IMemberService.cs
@@ -0,0 +1,19 @@
+﻿using MembersService.Domain.Entities;
+using System.Linq.Expressions;
+using System;
+
+namespace MembersService.Application.Interfaces;
+
+public interface IMemberService
+{
+    public Task<Member> AddAsync(Member entity);
+
+    public Task<IEnumerable<Member>> GetAllAsync();
+    public Task<Member> GetByIdAsync(int id);
+    public Task<IEnumerable<Member>> FindAsync(Expression<Func<Member, bool>> predicate);
+    public Task<Member> UpdateAsync(int id, Member entity);
+
+    public Task RemoveAsync(int id);
+
+    public Task RemoveMembersByTeamId(Expression<Func<Member, bool>> predicate, int id);
+}
