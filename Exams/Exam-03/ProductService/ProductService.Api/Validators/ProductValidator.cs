7c180b3e364ab95d859a70af1e125741e8b77265
davidhb7 <herrera17david@gmail.com>
2023-11-24 16:10:26 -0500

    Cambio de required

--- Exams/Exam-03/ProductService/ProductService.Api/Validators/ProductValidator.cs
+++ Exams/Exam-03/ProductService/ProductService.Api/Validators/ProductValidator.cs
@@ -10,33 +10,33 @@ namespace ProductService.Api.Validators
 
             RuleFor(p => p.CategoryId)
                 .NotEmpty()
-                .WithMessage("CategoryId is required");
+                .WithMessage("CategoryId is Required");
 
             RuleFor(p => p.CategoryName)
                 .NotEmpty()
-                .WithMessage("CategoryName is required")
+                .WithMessage("CategoryName is Required")
                 .MaximumLength(30)
                 .WithMessage("The maximum length of CategoryName is 30 characters");
 
             RuleFor(p => p.Price)
                 .NotEmpty()
-                .WithMessage("Price is required");
+                .WithMessage("Price is Required");
 
             RuleFor(p => p.Unit)
                 .NotEmpty()
-                .WithMessage("Unit is required")
+                .WithMessage("Unit is Required")
                 .MaximumLength(20)
                 .WithMessage("Unit's Max Length is 20 Characters");
 
             RuleFor(p => p.EanCode)
                 .NotEmpty()
-                .WithMessage("EanCode is required")
+                .WithMessage("EanCode is Required")
                 .MaximumLength(13)
                 .WithMessage("EanCode's Max Length is 13 digits");
 
             RuleFor(p => p.Description)
                 .NotEmpty()
-                .WithMessage("Description is required")
+                .WithMessage("Description is Required")
                 .MaximumLength(50)
                 .WithMessage("Description's Max Length is 50 Characters");
         }
