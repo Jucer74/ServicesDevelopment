9c049750f8d934b834a711e55424ec64b266be73
aflasso <andresfelipe.lasso@gmail.com>
2025-02-24 19:50:41 -0500

    Capa DAL y Entities implementada

---
+++ Practices/01.Layers/UserManagerApp/UserManagerApp.DAL/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+namespace UserManagerApp.DAL;
+
+public class UserRepository
+{
+    private static List<User> _users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public List<User> GetUsers()
+        {
+            return _users;
+        }
+}
