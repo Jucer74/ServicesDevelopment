a75b5bf47328e23233c437c5d91d5b901ca47124
ZCardonaZ <santiagocardonan0625@gmail.com>
2025-02-24 23:19:34 -0500

    Primer

---
+++ Practices/01.Layers/UserManagementApp/DAL/UserRepository.cs
@@ -0,0 +1,29 @@
+// DAL/UserRepository.cs
+using System.Collections.Generic;
+
+namespace UserManagementApp.DAL
+{
+    public class UserRepository
+    {
+        private static List<User> _users = new List<User>();
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public List<User> GetUsers()
+        {
+            return _users;
+        }
+    }
+
+    // Entity
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
+
