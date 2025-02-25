9042536a4dae3bf5aa095e7078e322f054d50081
aflasso <andresfelipe.lasso@gmail.com>
2025-02-24 21:17:48 -0500

    Separacion de acciones en el menor en funciones y validacion

--- Practices/01.Layers/UserManagerApp/UserManagerApp.UI/Program.cs
+++ Practices/01.Layers/UserManagerApp/UserManagerApp.UI/Program.cs
@@ -19,32 +19,23 @@ namespace UserManagementApp.UI
                 Console.WriteLine("2. Listar Usuarios");
                 Console.WriteLine("3. Salir");
                 Console.Write("Seleccione una opción: ");
+
                 string option = Console.ReadLine();
+
+                if (isInputValid(option) == false)
+                {
+                    Console.WriteLine("Se debe ingresar un número.");
+                    continue;
+
+                }
 
                 if (option == "1")
                 {
-                    Console.Write("Nombre: ");
-                    string name = Console.ReadLine();
-                    Console.Write("Email: ");
-                    string email = Console.ReadLine();
-
-                    User newUser = new User
-                    {
-                        Id = userService.GetUsers().Count + 1,
-                        Name = name,
-                        Email = email
-                    };
-
-                    userService.AddUser(newUser);
-                    Console.WriteLine("Usuario agregado con éxito!");
+                    addUser(userService);
                 }
                 else if (option == "2")
                 {
-                    var users = userService.GetUsers();
-                    foreach (var user in users)
-                    {
-                        Console.WriteLine($"ID: {user.Id}, Nombre: {user.Name}, Email: {user.Email}");
-                    }
+                    listUsers(userService);
                 }
                 else if (option == "3")
                 {
@@ -56,5 +47,41 @@ namespace UserManagementApp.UI
                 }
             }
         }
+
+        static public void addUser(UserService userService) {
+
+            Console.Write("Nombre: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            User newUser = new User
+            {
+                Id = userService.GetUsers().Count + 1,
+                Name = name,
+                Email = email
+            };
+
+            userService.AddUser(newUser);
+
+            Console.WriteLine("Usuario agregado con éxito!");
+
+        }
+
+        static public void listUsers(UserService userService) {
+
+            var users = userService.GetUsers();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"ID: {user.Id}, Nombre: {user.Name}, Email: {user.Email}");
+            }
+        }
+
+        static public bool isInputValid(string input) {
+
+            return int.TryParse(input, out _);
+        }
     }
+
 }
