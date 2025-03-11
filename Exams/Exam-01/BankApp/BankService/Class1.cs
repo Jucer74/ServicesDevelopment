df823852f5de09937ff40475754e4bcd22a1885e
polsiv <forenks1005@gmail.com>
2025-03-10 21:11:45 -0500

    Sections

---
+++ Exams/Exam-01/BankApp/BankService/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.Json;
+using Models;
+
+namespace BankService {
+    public class BankService 
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiUrl = "http://localhost:3000/api/v1";
+
+        public BankService()
+        {
+            _httpClient = new HttpClient(); 
+        }
+
+        public async Task<BankAccount> GetAccount(string accountNumber) 
+        {
+            var response = await _httpClient.GetAsync($"{ApiUrl}/accounts/{accountNumber}");
+            
+            if (!response.IsSuccessStatusCode) return null;
+            var json = await response.Content.ReadAsByteArrayAsync();
+            return JsonSerializer.Deserialize<BankAccount>(json); //take care
+        }
+
+        public async Task<bool> ExistsAccount(string accountNumber)
+        {
+            return await GetAccount(accountNumber) != null;
+        } 
+
+        public async Task<IBankAccount> CreateAccount(IBankAccount bankAccount)
+        {
+            if (await ExistsAccount(bankAccount.AccountNumber))
+            {
+                Console.WriteLine("Account already exists!");
+                return null;
+            }   
+
+            string json = JsonSerializer.Serialize(bankAccount);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync($"{ApiUrl}/accounts", content);
+            response.EnsureSuccessStatusCode();
+
+            return bankAccount;
+        }
+
+        public async Task<decimal?> GetBalance(string accountNumber)
+        {
+           var account = await GetAccount(accountNumber);
+           if (account == null)
+           {
+                Console.WriteLine("Account Not Found");
+                return null;
+           }
+
+           return account.BalanceAmount;
+        }
+       
+    }
+
+}
+
---
+++ Exams/Exam-01/BankApp/Models/Models.cs
@@ -0,0 +1,19 @@
+﻿namespace Models {
+      public interface IBankAccount
+    {
+        string AccountNumber { get; }
+        string AccountOwner { get; }
+        decimal BalanceAmount { get; }
+        int AccountType {get; }
+
+
+    }
+    public class BankAccount : IBankAccount
+    {
+    public string AccountNumber { get; private set; }
+    public string AccountOwner { get; private set; }
+    public decimal BalanceAmount { get; private set; }
+    public int AccountType { get; private set; }
+    }
+}
+
