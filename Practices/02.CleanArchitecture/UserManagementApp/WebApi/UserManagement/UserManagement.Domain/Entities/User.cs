4d6de5c4f3d196f318658aafb441cdd0738c7bde
Julio Robles <Jucer74@Hotmail.com>
2025-04-07 21:27:54 -0500

    Application

---
+++ Practices/02.CleanArchitecture/UserManagementApp/WebApi/UserManagement/UserManagement.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UserManagement.Domain.Common;
+
+namespace UserManagement.Domain.Entities;
+
+public class User : EntityBase
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Fullname { get; set; }
+
+    [Required]
+        public string Password { get; set; }
+
+    [Required]
+    public string Username { get; set; }
+}
