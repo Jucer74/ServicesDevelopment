e1227f9446d0eb34596097ca9713db64a1f5f82c
Jmtrivinop <131928328+Jmtrivinop@users.noreply.github.com>
2025-04-28 19:08:53 -0500

    Practica 2 V2

--- Practices/02.CleanArchitecture/UserManagementApp/WebApi/UserManagement/UserManagement.Api/Program.cs
+++ Practices/02.CleanArchitecture/UserManagementApp/WebApi/UserManagement/UserManagement.Api/Program.cs
@@ -1,11 +1,62 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Api.Extensions;
+using UserManagement.Api.Middleware;
+using UserManagement.App.Interfaces;
+using UserManagement.App.Services;
+using UserManagement.Infrastucture.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add DbContext
 
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    )
+);
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errorDetails = context.ConstructErrorMessages();
+        return new BadRequestObjectResult(errorDetails);
+    };
+});
+
+builder.Services.AddScoped<IUserServices, UserServices>();
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Modules
+builder.Services.AddCoreModules();
+builder.Services.AddInfrastructureModules();
+builder.Services.AddMappingModules();
+
+builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
 
 var app = builder.Build();
 
@@ -15,10 +66,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseDeveloperExceptionPage();  
+app.UseExceptionMiddleware();
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseRouting();
 
 app.UseAuthorization();
+
+
+
 
 app.MapControllers();
 
