410bd7467c3976e505fad10cf022e25d1528ba17
JoseOtero15 <joseotero1504@gmail.com>
2025-04-29 14:08:48 -0500

    delete error changes

--- Exams/Exam-02/PricatApp/Pricat.Application/DTOs/CategoryDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Pricat.Application.DTOs
-{
-    public class CategoryDto
-    {
-        public int Id { get; set; }
-        public string Description { get; set; } = null!;
-    }
-}
--- Exams/Exam-02/PricatApp/Pricat.Application/Interfaces/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-using Pricat.Application.Common;
-using Pricat.Domain.Entities;
-
-namespace Pricat.Application.Interfaces.Repositories
-{
-    public interface IProductRepository : IRepository<Products>
-    {
-    }
-}
