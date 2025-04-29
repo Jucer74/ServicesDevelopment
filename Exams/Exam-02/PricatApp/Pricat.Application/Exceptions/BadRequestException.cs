dd490ea8c89c6e33b1c39c2cd637ebd71652bda4
AleSalTovar <alejosal@tovarmail.com>
2025-04-29 02:27:26 -0500

    exception

--- Exams/Exam-02/PricatApp/Pricat.Application/Exceptions/BadRequestException.cs
+++ Exams/Exam-02/PricatApp/Pricat.Application/Exceptions/BadRequestException.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pricat.Application.Exceptions
 {
@@ -21,5 +17,30 @@ namespace Pricat.Application.Exceptions
             : base(message, innerException)
         {
         }
+
+        // Factory methods para claridad semántica
+        public static BadRequestException InvalidAccountNumber() =>
+            new BadRequestException("Account number must be exactly 10 digits.");
+
+        public static BadRequestException InvalidAccountType() =>
+            new BadRequestException("Account type must be either 'checking' or 'saving'.");
+
+        public static BadRequestException InvalidOwnerName() =>
+            new BadRequestException("Owner name must not exceed 50 characters.");
+
+        public static BadRequestException InvalidInitialBalance() =>
+            new BadRequestException("Initial balance must be greater than or equal to 0.");
+
+        public static BadRequestException InvalidTransactionAmount() =>
+            new BadRequestException("Transaction amount must be greater than 0.");
+
+        public static BadRequestException InvalidTransactionType() =>
+            new BadRequestException("Transaction type must be 'deposit' or 'withdrawal'.");
+
+        public static BadRequestException OverdraftLimitExceeded() =>
+            new BadRequestException("Overdraft limit exceeded for 'checking' account.");
+
+        public static BadRequestException InsufficientFunds() =>
+            new BadRequestException("Insufficient funds for 'saving' account.");
     }
 }
