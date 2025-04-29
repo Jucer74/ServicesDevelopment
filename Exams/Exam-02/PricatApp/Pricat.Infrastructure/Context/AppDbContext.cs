80dfb3982d9dd2298154d73306381ae41c8493f3
Sebastián López Montenegro <110605842+Sxl07@users.noreply.github.com>
2025-04-28 21:48:57 -0500

    falta api

---
+++ Exams/Exam-02/PricatApp/Pricat.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Pricat.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pricat.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
---
+++ Exams/Exam-02/PricatApp/Pricat.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pricat.Application.Interfaces.Repositories;
+using Pricat.Domain.Models;
+using Pricat.Infrastructure.Common;
+using Pricat.Infrastructure.Context;
+
+namespace Pricat.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+
+        public async Task<Category?> GetCategoryByIdIncludeProduct(int id)
+        {
+            return await _appDbContext.Categories.Include(m => m.Products).Where(t => t.Id == id).FirstOrDefaultAsync();
+        }
+    }
+}
