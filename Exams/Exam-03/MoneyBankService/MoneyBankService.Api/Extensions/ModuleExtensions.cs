3dc4c51dc2de7b184dd7fc77ae57a218b80603a8
polsiv <forenks1005@gmail.com>
2025-05-14 11:04:03 -0500

    exam

---
+++ Exams/Exam-03/MoneyBankService/MoneyBankService.Api/Extensions/ModuleExtensions.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MoneyBankService.Api.Dto;
+using MoneyBankService.Api.Mappers;
+using MoneyBankService.Api.Validators;
+using MoneyBankService.Application.Interfaces;
+using MoneyBankService.Application.Services;
+using MoneyBankService.Domain.Interfaces.Repositories;
+using MoneyBankService.Infrastructure.Repositories;
+
+namespace MoneyBankService.Api.Extensions;
+
+public static class ModuleExtensions
+{
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAccountService, AccountService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAccountRepository, AccountRepository>();
+
+        return services;
+    }
+
+
+    public static IServiceCollection AddMapping(this IServiceCollection services)
+    {
+        // Auto Mapper Configurations
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new MappingProfile());
+        });
+
+        IMapper mapper = mapperConfig.CreateMapper();
+        services.AddSingleton(mapper);
+
+        return services;
+    }
+
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<AccountDto>, AccountValidator>();
+
+        return services;
+    }
+}
