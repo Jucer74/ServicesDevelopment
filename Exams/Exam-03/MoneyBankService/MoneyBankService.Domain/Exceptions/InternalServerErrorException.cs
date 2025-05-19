3c48c9e8d333903b1fb0a9c31ef6e201b0c5654f
DevIan28 <imarangov@correo.usbcali.edu.co>
2025-05-18 21:16:26 -0500

    ex3

---
+++ Exams/Exam-03/MoneyBankService/MoneyBankService.Domain/Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MoneyBankService.Domain.Exceptions;
+
+[ExcludeFromCodeCoverage]
+public class InternalServerErrorException : BusinessException
+{
+    public InternalServerErrorException()
+    {
+    }
+
+    public InternalServerErrorException(string message) : base(message)
+    {
+    }
+
+    public InternalServerErrorException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
