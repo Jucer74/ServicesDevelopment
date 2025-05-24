96c01b4666b4eab788e1e379cc5d2f05450da596
v4nt3 <gisselvanessaq@yahoo.es>
2025-05-23 22:58:53 -0500

    DomainCheck

--- Exams/Exam-03/MoneyBankService/MoneyBankService.Domain/Entities/Account.cs
+++ Exams/Exam-03/MoneyBankService/MoneyBankService.Domain/Entities/Account.cs
@@ -4,5 +4,15 @@ namespace MoneyBankService.Domain.Entities
 {
     public class Account : EntityBase
     {
+        public char AccountType { get; set; } = 'A';
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public string AccountNumber { get; set; } = null!;
+
+        public string OwnerName { get; set; } = null!;
+        public decimal BalanceAmount { get; set; } = 0;
+
+        public decimal OverdraftAmount { get; set; } = 0;
     }
 }
