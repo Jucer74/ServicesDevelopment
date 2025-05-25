4d14461321f328cf0cbcb71c448ae533bf9845be
ZCardonaZ <santiagocardonan0625@gmail.com>
2025-05-25 12:06:13 -0500

    DDS

---
+++ Exams/Exam-03/MoneyBankService/MoneyBankService.Application/Dto/TransactionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MoneyBankService.Application.Dto 
+{
+    public class TransactionDto
+    {
+        public int Id { get; set; } 
+        public string AccountNumber { get; set; } = null!; //
+        public decimal ValueAmount { get; set; } //
+    }
+}
