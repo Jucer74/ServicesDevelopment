dc70513c606913d8e481bf0a93dcc3b200059428
Julio Robles <jucer74@hotmail.com>
2025-05-26 21:15:59 -0500

    Test

--- Exams/Exam-03/MoneyBankService/MoneyBankService.Api/Program.cs
+++ Exams/Exam-03/MoneyBankService/MoneyBankService.Api/Program.cs
@@ -3,32 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoneyBankService.Api.Extensions;
 using MoneyBankService.Api.Middleware;
-using System.Text.Json; 
 using MoneyBankService.Infrastructure.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add the DB Context
-var connectionString = builder.Configuration.GetConnectionString("CnnStr"); // Obt�n la cadena de conexi�n
+// Add DBContext
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)) // Sintaxis correcta para Pomelo
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("CnnStr"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("CnnStr"))
+    )
 );
-
 // Add services to the container.
-builder.Services.AddControllers()
-    .AddJsonOptions(options => 
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
     {
-        
-       // options.JsonSerializerOptions.PropertyNamingPolicy = null;
-    })
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.InvalidModelStateResponseFactory = context =>
-        {
-            var errorDetails = context.ConstructErrorMessages();
-            return new BadRequestObjectResult(errorDetails);
-        };
-    });
+        var errorDetails = context.ConstructErrorMessages();
+        return new BadRequestObjectResult(errorDetails);
+    };
+});
 
 // Add Fluent Validation
 builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
@@ -37,32 +32,34 @@ builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// 1. Configura la política de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://moneybankweb-brianddia.s3-website.us-east-2.amazonaws.com") // <--- Aquí pon la URL de tu frontend. Agrega también la pública cuando lo subas.
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add Modules
 builder.Services.AddRepositories();
 builder.Services.AddServices();
 builder.Services.AddMapping();
 builder.Services.AddValidators();
 
-
-builder.WebHost.UseUrls("http://*:80");
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors("AllowFrontend");
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Add the Exception Middleware Handler
 app.UseExceptionMiddleware();
 
 app.UseHttpsRedirection();
-
-app.UseDefaultFiles();
-
-app.UseStaticFiles();
 
 app.UseAuthorization();
 
