36cd50895e3bb241155271e3b75f6f704fca2a72
Juan Pizo <162081539+Juanpizoc@users.noreply.github.com>
2025-05-26 21:28:21 -0500

    AWS

--- Exams/Exam-03/MoneyBankService/MoneyBankService.Api/Program.cs
+++ Exams/Exam-03/MoneyBankService/MoneyBankService.Api/Program.cs
@@ -50,11 +50,19 @@ builder.Services.AddValidators();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//  app.UseSwagger();
+//app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoneyBankService API V1");
+    c.RoutePrefix = "swagger"; // Set Swagger UI at the app's root
+});
+
 
 // Add the Exception Middleware Handler
 app.UseExceptionMiddleware();
